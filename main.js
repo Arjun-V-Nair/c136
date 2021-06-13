74c05510f7f9a7cde40c407a4f46318c61fcf730
Arjun-V-Nair <72863311+Arjun-V-Nair@users.noreply.github.com>
2021-06-12 22:04:44 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+video = "";
+status = "";
+function preload(){
+video = createVideo("video.mp4");
+video.hide();
+}
+/*
+SPACE◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+*/
+function setup(){
+canvas = createCanvas(300, 380);
+canvas.center();
+}
+/*
+SPACE◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+*/
+function draw(){
+image(video, 0, 0, 300, 380)
+}
+/*
+SPACE◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+*/
+function start(){
+objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("status").innerHTML = "Status= Detecting Objects";                                                                                                                                            
+}
+/*
+SPACE◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+*/
+function modelLoaded(){
+console.log("MODEL IS LOADED!!!!!");
+status = true;
+video.loop();
+video.speed(1);
+video.volume(1);
+}
